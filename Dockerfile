62bb474225150d0d4deb62bfa22941cbf6b58c9b
kahootali <kahoot.ali@gmail.com>
2021-09-04 14:27:55 +0500

    update code to use html

--- Dockerfile
+++ Dockerfile
@@ -1,17 +1,19 @@
-FROM golang:alpine
+FROM golang:1.17.0 as build
+
+WORKDIR /app
+RUN go env -w GO111MODULE=auto 
+RUN go get github.com/thedevsaddam/renderer/...
+COPY main.go .
+RUN GOOS=linux GOARCH=amd64 CGO_ENABLED=0 go build main.go
+
+
+FROM alpine
 
 LABEL name="Golang Application" \    
       maintainer="Ali Kahoot <kahoot.ali@outlook.com>" \
       summary="A Golang Sample application"  
-
 WORKDIR /app
-
 EXPOSE 8080 
-
-COPY main.go .
-
-RUN go env -w GO111MODULE=auto 
-
-RUN go build
- 
-CMD ["./app"]
+COPY --from=build ./app/main ./
+COPY ./tpl ./tpl
+CMD ["./main"]
--- main.go
+++ main.go
@@ -3,36 +3,31 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"github.com/thedevsaddam/renderer"
 )
 
+var rnd *renderer.Render
+
+func init() {
+	opts := renderer.Options{
+		ParseGlobPattern: "./tpl/*.html",
+	}
+
+	rnd = renderer.New(opts)
+}
+
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "No Endpoint ")
-	})
-
-	http.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello, you've requested: %s\n", r.URL.Path)
-	})
-
-	http.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Status: UP")
-	})
-
-	http.HandleFunc("/batch-07", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "DevOps batch 07 will start on 11th Sept")
-	})
-	
-	http.HandleFunc("/webinar", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "")
-	})
-
-	i := 0
-
-	http.HandleFunc("/count", func(w http.ResponseWriter, r *http.Request) {
-		i = i + 1
-		fmt.Fprintf(w, "%d", i)
-	})
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", home)
+	mux.HandleFunc("/schedule", schedule)
 	fmt.Println("Server is Listening at 127.0.0.1:8080")
+	http.ListenAndServe(":8080", mux)
+}
 
-	http.ListenAndServe(":8080", nil)
+func home(w http.ResponseWriter, r *http.Request) {
+	rnd.HTML(w, http.StatusOK, "home", nil)
+}
+
+func schedule(w http.ResponseWriter, r *http.Request) {
+	rnd.HTML(w, http.StatusOK, "schedule", nil)
 }
