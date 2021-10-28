2e5a7f98560c3ca188d4740e2b658d595e906473
kahootali <kahoot.ali@gmail.com>
2021-10-28 17:08:58 +0500

    remove pushd command

--- deploy/scripts/deploy.sh
+++ deploy/scripts/deploy.sh
@@ -2,11 +2,10 @@ echo "Github Workspace: $GITHUB_WORKSPACE"
 echo "ls in chkk path"
 ls ${CHKK_INSTALL_PATH}
 echo "ls finish"
-pushd deploy || exit 1
 echo "adding helm repo"
 helm repo add tg https://tarabutgateway.github.io/application-helm/
 helm repo update
 echo "ls in chkk path"
 ls ${CHKK_INSTALL_PATH}
 echo "ls finish"
-helm upgrade --install --create-namespace -f helm-values/dev.yaml --set deployment.image.tag="${IMAGE_TAG}" -n dev golang-app-dev tg/application-helm --post-renderer=$GITHUB_WORKSPACE/bin/chkk-post-renderer
+helm upgrade --install --create-namespace -f deploy/helm-values/dev.yaml --set deployment.image.tag="${IMAGE_TAG}" -n dev golang-app-dev tg/application-helm --post-renderer=$GITHUB_WORKSPACE/bin/chkk-post-renderer
