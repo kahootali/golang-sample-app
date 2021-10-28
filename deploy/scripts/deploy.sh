e1d4658cd610c589695ad2bcfbb9a439df1e84db
kahootali <kahoot.ali@gmail.com>
2021-10-28 16:41:18 +0500

    fix repo dir

--- deploy/scripts/deploy.sh
+++ deploy/scripts/deploy.sh
@@ -1,5 +1,8 @@
+echo "ls in chkk path"
+ls ${CHKK_INSTALL_PATH}
 pushd deploy || exit 1
 export CHKK_ACCESS_TOKEN=${{ secrets.CHKK_TOKEN }}
+echo "adding helm repo"
 helm repo add tg https://tarabutgateway.github.io/application-helm/
 helm repo update
-helm upgrade --install --create-namespace -f helm-values/dev.yaml --set deployment.image.tag="${IMAGE_TAG}" -n dev golang-app-dev tg/application-helm --post-renderer=${CHKK_INSTALL_PATH}/chkk-post-renderer 
+helm upgrade --install --create-namespace -f helm-values/dev.yaml --set deployment.image.tag="${IMAGE_TAG}" -n dev golang-app-dev tg/application-helm --post-renderer=${CHKK_INSTALL_PATH}/chkk-post-renderer
