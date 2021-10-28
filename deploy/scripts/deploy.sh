80beaed25aab8bb5fa283edf65dcb4132a83c75d
kahootali <kahoot.ali@gmail.com>
2021-10-28 16:49:11 +0500

    fix token and add debug commands

--- deploy/scripts/deploy.sh
+++ deploy/scripts/deploy.sh
@@ -1,8 +1,12 @@
+echo "Github Workspace: $GITHUB_WORKSPACE"
 echo "ls in chkk path"
 ls ${CHKK_INSTALL_PATH}
+echo "ls finish"
 pushd deploy || exit 1
-export CHKK_ACCESS_TOKEN=${{ secrets.CHKK_TOKEN }}
 echo "adding helm repo"
 helm repo add tg https://tarabutgateway.github.io/application-helm/
 helm repo update
+echo "ls in chkk path"
+ls ${CHKK_INSTALL_PATH}
+echo "ls finish"
 helm upgrade --install --create-namespace -f helm-values/dev.yaml --set deployment.image.tag="${IMAGE_TAG}" -n dev golang-app-dev tg/application-helm --post-renderer=${CHKK_INSTALL_PATH}/chkk-post-renderer
