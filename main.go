b05f435484cc4bbca52dde3a4749aebe0f181553
kahootali <kahoot.ali@gmail.com>
2021-10-28 13:02:23 +0500

    use helm charts & add health check

--- main.go
+++ main.go
@@ -20,8 +20,14 @@ func main() {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", home)
 	mux.HandleFunc("/schedule", schedule)
+	mux.HandleFunc("/health", health)
 	fmt.Println("Server is Listening at 127.0.0.1:8080")
 	http.ListenAndServe(":8080", mux)
+}
+
+func health(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintf(w, "App is up and running")
 }
 
 func home(w http.ResponseWriter, r *http.Request) {
