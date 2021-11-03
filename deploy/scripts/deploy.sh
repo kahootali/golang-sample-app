2f15aecb6233bdd0d3f07a58df21cb5d317c218c
kahootali <kahoot.ali@gmail.com>
2021-11-03 11:45:03 +0500

    fix path for renderer

--- deploy/scripts/deploy.sh
+++ deploy/scripts/deploy.sh
@@ -2,4 +2,5 @@ echo "adding helm repo"
 helm repo add tg https://tarabutgateway.github.io/application-helm/
 helm repo update
 echo "Image Tag: ${IMAGE_TAG}"
-helm upgrade --install --create-namespace -f deploy/helm-values/dev.yaml --set deployment.image.tag="${IMAGE_TAG}" -n dev golang-app-dev tg/application-helm --post-renderer=${CHKK_INSTALL_PATH}/chkk-post-renderer
+helm upgrade --install --create-namespace -f deploy/helm-values/dev.yaml --set deployment.image.tag="${IMAGE_TAG}" -n dev golang-app-dev tg/application-helm --post-renderer=./chkk-post-renderer
+
